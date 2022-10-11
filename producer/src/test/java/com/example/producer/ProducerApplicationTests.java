8da15ae6a0175bf92a08383b7caad39ea4bb0322
ch <mkmsacc@outlook.com>
2022-10-11 16:05:57 +0700

    Add integration test for consumer

--- producer/src/test/java/com/example/producer/ProducerApplicationTests.java
+++ producer/src/test/java/com/example/producer/ProducerApplicationTests.java
@@ -2,31 +2,57 @@ package com.example.producer;
 
 import com.example.producer.domain.Book;
 import com.example.producer.domain.LibraryEvent;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@EmbeddedKafka(topics = {"library-event"})
+@EmbeddedKafka(topics = {"library-events"})
 @TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
         "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"})
 class ProducerApplicationTests {
 
-
     @Autowired
     private WebTestClient webTestClient;
 
+    private Consumer<Integer, String> consumer;
+
+    @BeforeEach
+    void setUp(@Autowired EmbeddedKafkaBroker embeddedKafkaBroker) {
+        var configs = KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker);
+        consumer = new DefaultKafkaConsumerFactory<>(configs, new IntegerDeserializer(), new StringDeserializer()).createConsumer();
+        embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+    }
+
+    @AfterEach
+    void tearDown() {
+        consumer.close();
+    }
+
     @Test
     void shouldReturnLibraryEvent() {
-        Book book = Book.builder()
+        var book = Book.builder()
                 .id(1)
                 .author("author")
                 .name("name")
                 .build();
-        LibraryEvent request = LibraryEvent.builder()
+        var request = LibraryEvent.builder()
                 .id(1)
                 .book(book)
                 .build();
@@ -38,5 +64,9 @@ class ProducerApplicationTests {
                 .expectStatus()
                 .isCreated();
 
+        var singleRecord = KafkaTestUtils.getSingleRecord(consumer, "library-events");
+        assertThat(singleRecord.value()).isNotNull();
+
     }
+
 }
