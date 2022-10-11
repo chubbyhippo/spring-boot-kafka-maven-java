2ce2da750db625e58b730766858589101936815f
ch <mkmsacc@outlook.com>
2022-10-11 15:00:32 +0700

    Add integration test

--- producer/src/test/java/com/example/producer/ProducerApplicationTests.java
+++ producer/src/test/java/com/example/producer/ProducerApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.producer;
 
+import com.example.producer.domain.Book;
+import com.example.producer.domain.LibraryEvent;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = {"library-event"})
+@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"})
 class ProducerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void shouldReturnLibraryEvent() {
+        Book book = Book.builder()
+                .id(1)
+                .author("author")
+                .name("name")
+                .build();
+        LibraryEvent request = LibraryEvent.builder()
+                .id(1)
+                .book(book)
+                .build();
+
+        webTestClient.post()
+                .uri("/v1/libraryevent")
+                .bodyValue(request)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+
+    }
 }
