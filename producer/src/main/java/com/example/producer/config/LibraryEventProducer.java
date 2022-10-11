760d29a9cfa94742fba83e9077487bed46d83360
ch <mkmsacc@outlook.com>
2022-10-11 12:19:06 +0700

    Implement controller

---
+++ producer/src/main/java/com/example/producer/config/LibraryEventProducer.java
@@ -0,0 +1,54 @@
+package com.example.producer.config;
+
+import com.example.producer.domain.LibraryEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class LibraryEventProducer {
+    private final KafkaTemplate<Integer, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
+        var key = libraryEvent.getId();
+        var value = objectMapper.writeValueAsString(libraryEvent);
+        ListenableFuture<SendResult<Integer, String>> sendResultListenableFuture = kafkaTemplate.sendDefault(key, value);
+        sendResultListenableFuture.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onFailure(@NonNull Throwable ex) {
+                handleFailure(key, value, ex);
+
+            }
+
+            @Override
+            public void onSuccess(SendResult<Integer, String> result) {
+                handleSuccess(key, value, result);
+            }
+
+        });
+
+    }
+
+    private void handleFailure(Integer key, String value, Throwable ex) {
+
+        log.error("Error sending the message for key : {}, and value is {},and the exception is {}", key, value,
+                ex.getMessage());
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
+        log.info("Message has been sent successfully for the key : {}, and the value is {}, and partition is {}", key,
+                value, result.getRecordMetadata().partition());
+    }
+
+
+}
--- producer/src/main/java/com/example/producer/controller/LibraryEventController.java
+++ producer/src/main/java/com/example/producer/controller/LibraryEventController.java
@@ -1,6 +1,9 @@
 package com.example.producer.controller;
 
+import com.example.producer.config.LibraryEventProducer;
 import com.example.producer.domain.LibraryEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,11 +11,16 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class LibraryEventController {
+
+    private final LibraryEventProducer libraryEventProducer;
 
     @PostMapping("/v1/libraryevent")
     @ResponseStatus(HttpStatus.CREATED)
-    public LibraryEvent postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+
+    public LibraryEvent postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
         return libraryEvent;
     }
 }
