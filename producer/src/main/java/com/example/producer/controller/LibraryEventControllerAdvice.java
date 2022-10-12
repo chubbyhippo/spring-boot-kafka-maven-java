6fa7d31c08d7459037d0dcc40914ff83b68e92af
ch <mkmsacc@outlook.com>
2022-10-12 08:36:21 +0700

    Add controller advice

---
+++ producer/src/main/java/com/example/producer/controller/LibraryEventControllerAdvice.java
@@ -0,0 +1,26 @@
+package com.example.producer.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+@ResponseBody
+public class LibraryEventControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleRequestBody(MethodArgumentNotValidException exception) {
+        var errors = exception.getBindingResult().getFieldErrors();
+        return errors.stream()
+                .map(fieldError -> fieldError.getField() + " - " + fieldError.getDefaultMessage())
+                .sorted()
+                .collect(Collectors.joining(", "));
+    }
+
+}
