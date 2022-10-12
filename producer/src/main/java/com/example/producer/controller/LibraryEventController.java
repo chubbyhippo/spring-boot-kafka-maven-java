8db14def28bd541ff4ee30382223b95153cc7c1b
ch <mkmsacc@outlook.com>
2022-10-12 08:36:12 +0700

    Add annotations

--- producer/src/main/java/com/example/producer/controller/LibraryEventController.java
+++ producer/src/main/java/com/example/producer/controller/LibraryEventController.java
@@ -6,14 +6,16 @@ import com.example.producer.domain.LibraryEventType;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@RestController
+@Controller
+@ResponseBody
 @RequiredArgsConstructor
 public class LibraryEventController {
 
@@ -21,7 +23,6 @@ public class LibraryEventController {
 
     @PostMapping("/v1/libraryevent")
     @ResponseStatus(HttpStatus.CREATED)
-
     public LibraryEvent postLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException {
         libraryEvent.setLibraryEventType(LibraryEventType.NEW);
         libraryEventProducer.sendLibraryEvent(libraryEvent);
