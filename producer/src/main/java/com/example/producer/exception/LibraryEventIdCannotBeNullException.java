3e0965a12dbe36782a71cc55f3c544fdcfe2e431
ch <mkmsacc@outlook.com>
2022-10-12 14:38:16 +0700

    Add library event cannot be null exception

---
+++ producer/src/main/java/com/example/producer/exception/LibraryEventIdCannotBeNullException.java
@@ -0,0 +1,8 @@
+package com.example.producer.exception;
+
+public class LibraryEventIdCannotBeNullException extends Exception {
+
+    public LibraryEventIdCannotBeNullException(String s) {
+        super(s);
+    }
+}
