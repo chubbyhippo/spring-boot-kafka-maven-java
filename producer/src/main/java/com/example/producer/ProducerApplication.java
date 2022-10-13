ab488aa68aa28834eb9aa30dd71a8966eec01550
ch <mkmsacc@outlook.com>
2022-10-13 17:56:34 +0700

    Setting kafka admin

--- producer/src/main/java/com/example/producer/ProducerApplication.java
+++ producer/src/main/java/com/example/producer/ProducerApplication.java
@@ -1,7 +1,15 @@
 package com.example.producer;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class ProducerApplication {
@@ -9,5 +17,19 @@ public class ProducerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerApplication.class, args);
 	}
+    @Bean
+    public KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092,localhost:39092");
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("library-events")
+                .partitions(10)
+                .build();
+    }
+
 
 }
