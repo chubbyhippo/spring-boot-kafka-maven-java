2275419432c931c4fc4dd7bf8fe2d70da62f6f2c
ch <mkmsacc@outlook.com>
2022-10-13 17:56:06 +0700

    Delete LibraryEventsConsumerConfig.java

--- consumer/src/main/java/com/example/consumer/config/LibraryEventsConsumerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.consumer.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
-
-@Configuration
-@EnableKafka
-public class LibraryEventsConsumerConfig {
-}
