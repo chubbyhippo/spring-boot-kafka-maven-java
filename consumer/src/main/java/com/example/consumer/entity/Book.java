8c5ae68c0c43a5aee6a2b62630b6f517d164a032
ch <mkmsacc@outlook.com>
2022-10-14 10:45:45 +0700

    Add book

---
+++ consumer/src/main/java/com/example/consumer/entity/Book.java
@@ -0,0 +1,22 @@
+package com.example.consumer.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Book {
+    @NotNull
+    private Integer id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String author;
+}
