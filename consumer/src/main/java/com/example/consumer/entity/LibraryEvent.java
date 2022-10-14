10003fa4f996ba1e037c6876c778f26f60c606b0
ch <mkmsacc@outlook.com>
2022-10-14 10:45:56 +0700

    Add LibraryEvent

---
+++ consumer/src/main/java/com/example/consumer/entity/LibraryEvent.java
@@ -0,0 +1,21 @@
+package com.example.consumer.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class LibraryEvent {
+    private Integer id;
+    private LibraryEventType libraryEventType;
+    @NotNull
+    @Valid
+    private Book book;
+}
