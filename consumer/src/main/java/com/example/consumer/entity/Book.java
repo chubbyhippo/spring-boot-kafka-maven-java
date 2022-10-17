d233ed0e06363ecdcef9236061f2b0ff19ccb6cd
ch <mkmsacc@outlook.com>
2022-10-17 10:46:48 +0700

    Update Book

--- consumer/src/main/java/com/example/consumer/entity/Book.java
+++ consumer/src/main/java/com/example/consumer/entity/Book.java
@@ -1,22 +1,32 @@
 package com.example.consumer.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
 @Builder
+@Entity
 public class Book {
+    @Id
     @NotNull
     private Integer id;
     @NotBlank
     private String name;
     @NotBlank
     private String author;
+
+    @OneToOne
+    @JoinColumn(name = "id")
+    private LibraryEvent libraryEvent;
+
 }
