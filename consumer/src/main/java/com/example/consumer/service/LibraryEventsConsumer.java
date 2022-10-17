d6d4b70b00e5ec595effb56c41e5414cc42c9ccf
ch <mkmsacc@outlook.com>
2022-10-17 13:02:38 +0700

    Add service to consumer

--- consumer/src/main/java/com/example/consumer/service/LibraryEventsConsumer.java
+++ consumer/src/main/java/com/example/consumer/service/LibraryEventsConsumer.java
@@ -1,5 +1,7 @@
 package com.example.consumer.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -7,10 +9,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class LibraryEventsConsumer {
 
+    private final LibraryEventService libraryEventService;
+
     @KafkaListener(topics = {"library-events"})
-    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) {
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
         log.info("ConsumerRecord : {} ", consumerRecord);
+        libraryEventService.processLibraryEvent(consumerRecord);
     }
 }
