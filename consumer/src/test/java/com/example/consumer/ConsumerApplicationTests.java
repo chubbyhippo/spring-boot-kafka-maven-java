c55704e8d846c5ca0e5c870d1665612bf87b8340
ch <mkmsacc@outlook.com>
2022-10-25 17:58:41 +0700

    Rename to update

--- consumer/src/test/java/com/example/consumer/ConsumerApplicationTests.java
+++ consumer/src/test/java/com/example/consumer/ConsumerApplicationTests.java
@@ -83,7 +83,7 @@ class ConsumerApplicationTests {
     }
 
     @Test
-    void shouldShowErrorPublishNewLibraryEvent() throws ExecutionException, InterruptedException {
+    void shouldShowErrorPublishUpdateLibraryEvent() throws ExecutionException, InterruptedException {
         var json = """
                     {
                     	"id": null,
