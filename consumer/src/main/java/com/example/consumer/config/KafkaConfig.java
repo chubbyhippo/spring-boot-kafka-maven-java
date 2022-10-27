bc6e58d3797fe9913681c40721685185548bce78
ch <mkmsacc@outlook.com>
2022-10-27 22:29:05 +0700

    Implement kafka config

---
+++ consumer/src/main/java/com/example/consumer/config/KafkaConfig.java
@@ -0,0 +1,27 @@
+package com.example.consumer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<Integer, String>
+    kafkaListenerContainerFactory(ConsumerFactory<Integer, String> consumerFactory) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<Integer, String>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setCommonErrorHandler(customError());
+        return factory;
+    }
+
+
+    public DefaultErrorHandler customError() {
+        var fixedBackOff = new FixedBackOff(1000L, 2L);
+        return new DefaultErrorHandler(fixedBackOff);
+    }
+}
