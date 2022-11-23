28ce09cb4cc6dbda27bc4eaf561d36027b69848f
ch <mkmsacc@outlook.com>
2022-11-23 15:06:45 +0700

    Add retry consumer

---
+++ consumer/src/main/java/com/example/consumer/service/LibraryEventsRetryConsumer.java
@@ -0,0 +1,23 @@
+package com.example.consumer.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class LibraryEventsRetryConsumer {
+    private final LibraryEventService libraryEventService;
+
+    @KafkaListener(topics = {"${topics.retry}"}, groupId = "retry-listener-group")
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
+        log.info("ConsumerRecord in Retry consumer: {} ", consumerRecord);
+        consumerRecord.headers()
+                .forEach(header -> log.info("Key : {}, Value : {}", header.key(), new String(header.value())));
+        libraryEventService.processLibraryEvent(consumerRecord);
+    }
+}
