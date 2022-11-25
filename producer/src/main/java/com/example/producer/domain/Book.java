c38c79d83278dbb1355341ca6c99bd1983f23d6f
ch <mkmsacc@outlook.com>
2022-11-25 15:30:28 +0700

    Update to spring boot 3.0.0

--- producer/src/main/java/com/example/producer/domain/Book.java
+++ producer/src/main/java/com/example/producer/domain/Book.java
@@ -1,12 +1,12 @@
 package com.example.producer.domain;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
